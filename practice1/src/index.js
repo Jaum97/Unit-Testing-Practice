c168411149c3b068d74c96bb9b77c9d2ca543c25
ChilenoSika <jaum97@yahoo.com.br>
2019-04-04 23:44:34 -0300

    Setting up Jest

--- practice1/src/index.js
+++ practice1/src/index.js
@@ -3,9 +3,17 @@ if (
     items: [{ name: 'Dragon food', price: 8 }, { name: 'Dragon cage (small)', price: 800 }],
   }) !== 808
 ) {
-  throw new Error('Check fail: Happy path');
+  throw new Error('Check fail: Happy path (Example 1)');
 }
 
-function orderTotal() {
-  return 808;
+if (
+  orderTotal({
+    items: [{ name: 'Dragon Collar', price: 20 }, { name: 'Dragon chew toy', price: 40 }],
+  }) !== 60
+) {
+  throw new Error('Check fail: Happy path Example 2');
+}
+
+function orderTotal(order) {
+  return order.items.reduce((prev, cur) => cur.price + prev, 0);
 }
